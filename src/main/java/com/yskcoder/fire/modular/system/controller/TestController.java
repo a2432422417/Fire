466849e6fccc82e357221346358df40db23d9962
yunshunkun <1061156525@qq.com>
2018-07-04 11:13:14 +0800

    Initial commit

---
+++ src/main/java/com/yskcoder/fire/modular/system/controller/TestController.java
@@ -0,0 +1,13 @@
+package com.yskcoder.fire.modular.system.controller;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * @Auther: yskcoder
+ * @Date: 2018/7/3 17:55
+ * @Description:Controller测试类
+ */
+@Controller
+public class TestController {
+
+}
